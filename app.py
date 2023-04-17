fdf527db3d9bb777caf64f53ead2d66b40d35cbf
davy00ps <122569427+davy00ps@users.noreply.github.com>
2023-04-16 22:13:13 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import flask
+app = flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route("/"how are you)
+def hello():
+    return "I'm good, how about you?"
+
+if __name__ == "__main__"
+    app.run(host="0.0.0.0", port=8080)
